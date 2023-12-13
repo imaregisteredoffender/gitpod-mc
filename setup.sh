7fa4be2b5a5e31502876cecd8e6bc3f69dbd1b1a
ariez <121648764+imaregisteredoffender@users.noreply.github.com>
2023-12-13 22:56:50 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 sudo apt-get install openjdk-8-jre
 wget https://launcher.mojang.com/v1/objects/b58b2ceb36e01bcd8dbf49c8fb66c55a9f0676cd/server.jar
-java -Xms16G -Xmx16G -jar server.jar nogui
+echo setup complete
