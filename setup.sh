246f3f61cca86a6db19b4fbd04f73b83e1098bbd
ariez <121648764+imaregisteredoffender@users.noreply.github.com>
2023-12-13 22:59:45 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 sudo apt-get install openjdk-8-jre
 wget https://launcher.mojang.com/v1/objects/b58b2ceb36e01bcd8dbf49c8fb66c55a9f0676cd/server.jar
-echo setup complete
+java -Xms16G -Xmx16G -jar server.jar nogui
+echo Crash detected. If you did a exit on the server you can ignore this message.
