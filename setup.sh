e725f53e38f6a54b31ff3e71448f7e3501b09d0f
ariez <121648764+imaregisteredoffender@users.noreply.github.com>
2023-12-14 18:08:29 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-echo made for gitpod by ariez <3
+echo made for gitpod by ariez 
 sudo apt update
-sudo apt-get install openjdk-8-jre
+sudo apt-get install openjdk-8-jre -y
 wget https://launcher.mojang.com/v1/objects/b58b2ceb36e01bcd8dbf49c8fb66c55a9f0676cd/server.jar
 java -Xms16G -Xmx16G -jar server.jar nogui
 echo goodbye
