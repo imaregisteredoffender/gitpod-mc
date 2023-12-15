bbdb7ac67bb2f324574e20b501c562490b181af7
ariez <121648764+imaregisteredoffender@users.noreply.github.com>
2023-12-15 17:31:46 +0000

    Create playit.sh

---
+++ playit.sh
@@ -0,0 +1,7 @@
+echo made by ariez for gitpod
+echo copyright 2023 ariez inc
+curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
+sudo apt update
+sudo apt install playit -y
+run with run-playit.sh
