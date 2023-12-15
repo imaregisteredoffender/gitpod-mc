5803b40a345756161a1b546b138d8524bc6bf1ab
ariez <121648764+imaregisteredoffender@users.noreply.github.com>
2023-12-15 17:33:36 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ echo made for gitpod by ariez
 sudo apt update
 sudo apt-get install openjdk-8-jre -y
 wget https://launcher.mojang.com/v1/objects/b58b2ceb36e01bcd8dbf49c8fb66c55a9f0676cd/server.jar
-java -Xms16G -Xmx16G -jar server.jar nogui
-echo goodbye
+bash playit.sh
+echo Run the server with server.sh and run playit with /bin/playit
