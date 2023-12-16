16f23bc2d221eeab8f4043eb662b8e4e17def3bd
ariez <121648764+imaregisteredoffender@users.noreply.github.com>
2023-12-16 09:44:26 +0000

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,2 +1,10 @@
-echo Using 16GB Ram to modify please do nano / vi / vim server.sh
-java -Xms16G -Xmx16G -jar server.jar nogui
+#!/bin/bash
+
+# Prompt the user for the server.jar file name
+read -p "Enter the name of your server.jar (Press Enter for default 'server.jar'): " server_jar
+
+# Use the default value 'server.jar' if no input provided
+server_jar=${server_jar:-server.jar}
+
+# Run the server with the specified server.jar file
+sudo java -Xms16G -Xmx16G -jar "$server_jar" nogui
